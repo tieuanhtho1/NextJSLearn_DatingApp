b59f34c0c0766ed800dd1f87ac01da5c7832a0b4
unknown <tieuanhtho@gmail.com>
2024-07-27 20:41:34 +0700

    wip

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { getMembers } from '../actions/memberActions'
+import MemberCard from './MemberCard';
 
-export default function MembersPage() {
+export default async function MembersPage() {
+  const members = await getMembers();
+
   return (
-    <div>
-        aaaaaaaaaa
+    <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8'>
+          {members && members.map(member => (
+            <MemberCard key={member.id} member={member}/>
+          ))}
     </div>
   )
 }
